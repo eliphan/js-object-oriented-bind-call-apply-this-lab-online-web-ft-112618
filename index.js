3cfbcf57db4f42dffd2a10e7cc1e046e2dcad472
Elise Phan <aliceconnic@gmail.com>
2019-03-16 20:55:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ function setThisWithApply(fn, thisValue, ourArguments) {
   return fn.apply(thisValue, ourArguments);
 }
 
-
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const copy = functionToBeCopied.bind(thisValue);
+  return copy;
+}
